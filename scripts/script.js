d130c96e14e3ee4258d736eec9314a99d62e47f3
Kitsor3000 <krostik2007@gmail.com>
2025-03-04 14:12:30 +0200

    end 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loadingBar = document.getElementById("loading-bar");
 
-    // Починаємо анімацію полоски при завантаженні сторінки
+    
     loadingBar.style.width = "100%";
 
-    // Після того, як сторінка завантажиться, закриваємо полоску
+    
     window.addEventListener("load", function () {
         setTimeout(function () {
-            loadingBar.style.width = "0%"; // Зменшуємо полоску
-        }, 500); // Затримка перед приховуванням
+            loadingBar.style.width = "0%"; 
+        }, 500); 
     });
 
-    // Використовуємо beforeunload, щоб показати полоску при переході на іншу сторінку
+    
     window.addEventListener("beforeunload", function () {
-        loadingBar.style.width = "100%"; // Коли перехід починається
+        loadingBar.style.width = "100%"; 
     });
 });
